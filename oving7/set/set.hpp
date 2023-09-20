0eeafcdeae96fa5d5ec4e350574e035385ada555
Joakim Falch <joakimfalch@outlook.com>
2023-09-20 17:01:36 +0200

    Finished øving 7

---
+++ oving7/set/set.hpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <iostream>
+
+class Set {
+private:
+    std::vector<int> elements;
+
+public:
+    Set();
+    Set(const std::vector<int>& vec);
+
+    Set operator+(const Set& other) const;
+    Set& operator=(const Set& other);
+
+    void addElement(int element);
+    void print() const;
+};
